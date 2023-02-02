ef10696c4917aec94d45c4273cad1d54c81f207b
munishconceptbiu <munishwaran.m@conceptbiu.com>
2023-02-02 15:40:21 +0530

    check error

--- api/artical/artical.service.js
+++ api/artical/artical.service.js
@@ -64,7 +64,14 @@ async function getAll(client_id, article_id, entity_name, media_type) {
     let result;
     if (media_type === 'Print') {
 
-
+        console.log(`SELECT cav.id AS cav_id,cav.client_id,cav.article_id,cav.entity_id
+        ,a.publication_id,a.edition_id,a.publication_type_id,a.language_id,a.suppliment_id,a.source_id
+        ,e1.zone_id,cav.prominent_id,a.section_id
+      FROM client_article_values cav 
+        INNER JOIN entities e ON e.id=cav.entity_id
+        INNER JOIN articles a ON a.id=cav.article_id
+        LEFT JOIN editions e1 ON a.edition_id = e1.id
+      WHERE cav.article_id=${article_id} AND e.entity_name=${entity_name} AND cav.client_id=${client_id}`)
 
         result = await db2.sequelize.query(
             `SELECT cav.id AS cav_id,cav.client_id,cav.article_id,cav.entity_id
@@ -87,6 +94,13 @@ async function getAll(client_id, article_id, entity_name, media_type) {
         // });
     }
     if (media_type === 'Online') {
+        console.log(`SELECT cav.id AS cav_id,cav.client_id,cav.article_id,cav.entity_id
+        ,a.publication_id,a.publication_type_id,a.language_id,a.source_id
+        ,cav.prominent_id
+      FROM online_client_article_values cav 
+        INNER JOIN entities e ON e.id=cav.entity_id
+        INNER JOIN online_articles a ON a.id=cav.article_id
+      WHERE cav.article_id=${article_id} AND e.entity_name=${entity_name} AND cav.client_id=${client_id}`)
         result = await db2.sequelize.query(
             `SELECT cav.id AS cav_id,cav.client_id,cav.article_id,cav.entity_id
             ,a.publication_id,a.publication_type_id,a.language_id,a.source_id
