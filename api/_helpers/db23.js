1ba4aeb2ef9c48735b9daf2873ddb5f98a9d9eff
munishconceptbiu <munishwaran.m@conceptbiu.com>
2023-02-07 10:21:50 +0530

    changes db 2023

---
+++ api/_helpers/db23.js
@@ -0,0 +1,35 @@
+const config = require('config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db2 = {};
+
+initialize();
+
+async function initialize() {
+    // create db2 if it doesn't already exist
+    const { host, port, user, password, database } = config.livedatabase23;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    // const connection = await mysql.createConnection({host: host, user: user, database: database});
+    
+
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    // connect to db2
+    const sequelize = new Sequelize(database, user, password, { logging: false, host : host, dialect: 'mysql' , pool: {
+        max: 20,
+        min: 0,
+        acquire: 60000000,
+        idle: 1000000000
+      }});
+
+    // // init models and add them to the exported db2 object
+    db2.OnlineData = require('../_model/online.model')(sequelize);
+    db2.PrintData = require('../_model/print.model')(sequelize);
+      
+    db2.sequelize = sequelize;
+    db2.Sequelize = Sequelize;
+    // sync all models with database
+    await sequelize.sync();
+    
+}
