cae87851a46bbde9e32fa412cbf6f19beaad7e71
munishconceptbiu <munishwaran.m@conceptbiu.com>
2023-02-08 12:37:01 +0530

    changes

--- api/_helpers/db23.js
+++ api/_helpers/db23.js
@@ -2,7 +2,7 @@ const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
-module.exports = db2 = {};
+module.exports = db23 = {};
 
 initialize();
 
