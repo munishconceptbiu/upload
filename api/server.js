ba1d2d6e661b9e3f4d51d1a3baa638f8def89e94
munishconceptbiu <munishwaran.m@conceptbiu.com>
2023-02-13 14:29:01 +0530

    port check

--- api/server.js
+++ api/server.js
@@ -29,5 +29,5 @@ app.use('/themes', require('./themes/themes.router'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4001;
 app.listen(port, () => console.log('Server listening on port ' + port));
