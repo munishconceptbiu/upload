374d560beae1bb766673054e0022c49a3333ad50
munishconceptbiu <munishwaran.m@conceptbiu.com>
2023-02-22 09:41:42 +0530

    chagnes

--- api/editions/edition.router.js
+++ api/editions/edition.router.js
@@ -7,6 +7,6 @@ const userController = require('../editions/edition.controller')
 // router
 router.get('/zone/:zoneid',  userController.getZoneEdition);
 
-router.get('/:name',  userController.getEdition);
+router.get('/name/:name',  userController.getEdition);
 
 module.exports = router;
